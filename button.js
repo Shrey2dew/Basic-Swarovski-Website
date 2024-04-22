05a78308e3ec57b4fb9f4abfa1c58b8bca09f8ce
Shrey Nalode <117361844+Shrey2dew@users.noreply.github.com>
2024-04-23 01:53:37 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,5 @@
+function my(){
+    document.getElementById("demo").innerHTML = "<br><br><br>Thank you for your response<br><br><br><br>";
+    document.getElementById("demo").style.fontFamily = "serif";
+    document.getElementById("demo").style.fontSize = "30px";
+}
